f387b5e9416931df5322a3c8b209a172b462969b
Rajeev Mishra <113230206+Itsrajeev123@users.noreply.github.com>
2026-01-17 10:09:04 -0500

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Custom Email Throttle',
+    'version': '2.0',
+    'category': 'Marketing/Email Marketing',
+    'summary': 'Send emails in controlled batches with time delay',
+    'depends': ['mail', 'mass_mailing'],
+    'data': [
+        'views/mailing_mailing_views.xml',  # Add this line
+        'data/ir_cron_data.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
