258433b5eb256ce320e11853794746c7c1b12049
Rajeev Mishra <113230206+Itsrajeev123@users.noreply.github.com>
2026-01-17 10:36:21 -0500

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
     'summary': 'Send emails in controlled batches with time delay',
     'depends': ['mail', 'mass_mailing'],
     'data': [
-        'views/mailing_mailing_views.xml',  # Add this line
+        'views/mailing_mailing_views.xml', 
         'data/ir_cron_data.xml',
     ],
     'installable': True,
